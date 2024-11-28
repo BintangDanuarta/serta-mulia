c98bcfdb1363393208b0a7e3600078c40afd55e1
Bintang Danuarta <bintangdanuarta@outlook.com>
2024-11-29 02:25:33 +0700

    Push code to remote repository

---
+++ src/services/storeData.js
@@ -0,0 +1,11 @@
+const { Firestore } = require('@google-cloud/firestore');
+
+async function storeData(id, data) {
+    // Sesuaikan databaseId dengan instances yang dibuat di GCP
+    const db = new Firestore({ databaseId: "serta-mulia" });
+
+    const predictCollection = db.collection('prediction');
+    return predictCollection.doc(id).set(data);
+}
+
+module.exports = storeData;
